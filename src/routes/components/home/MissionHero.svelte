8585a9cab826d2b1a1eb6d5fac19f3e0b0936f0d
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-02-10 17:07:44 +0900

    모델 렌더링

--- src/routes/components/home/MissionHero.svelte
+++ src/routes/components/home/MissionHero.svelte
@@ -2,11 +2,64 @@
 	import * as THREE from 'three';
 	import { onMount } from 'svelte';
 	import { GLTFLoader, type GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+	const CanvasId = 'logo-canvas';
+
+	onMount(() => {
+		const canvas = document.getElementById(CanvasId);
+		if (!canvas) return;
+
+		const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+
+		const scene = new THREE.Scene();
+		const camera = new THREE.PerspectiveCamera(
+			45,
+			window.innerWidth / (window.innerHeight - 80),
+			1,
+			1000
+		);
+		camera.position.set(0, 0, 48);
+
+		const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
+		ambientLight.castShadow = true;
+		scene.add(ambientLight);
+		const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+		directionalLight.position.set(0, 32, 64);
+		scene.add(directionalLight);
+
+		window.addEventListener('resize', onWindowResize, false);
+
+		let loadedModel: GLTF;
+		const loader = new GLTFLoader();
+		loader.load('/assets/models/shiba/scene.gltf', (gltf) => {
+			loadedModel = gltf;
+			scene.add(loadedModel.scene);
+			loadedModel.scene.scale.set(10, 10, 10);
+			// loadedModel.scene.position.set(0, 0, 0);
+			// loadedModel.scene.rotation.set(0, 0, 0);
+		});
+
+		function onWindowResize() {
+			camera.aspect = window.innerWidth / (window.innerHeight - 80);
+			camera.updateProjectionMatrix();
+			renderer.setSize(window.innerWidth, window.innerHeight - 80);
+		}
+
+		function animate() {
+			requestAnimationFrame(animate);
+			// if (loadedModel) {
+			// 	loadedModel.scene.rotation.y += 0.01;
+			// }
+			renderer.render(scene, camera);
+		}
+		animate();
+	});
 </script>
 
 <section id="mission-section">
 	<span class="first">Shape the Future of Data</span>
 	<span class="second">with a Community of Enthusiasts</span>
+	<canvas id={CanvasId} />
 </section>
 
 <style>
@@ -27,8 +80,16 @@
 		font-weight: 600;
 	}
 
+	canvas {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: calc(100vh - 5rem);
+	}
+
 	span {
-		/* z-index: 10; */
+		z-index: 10;
 	}
 
 	.first {
