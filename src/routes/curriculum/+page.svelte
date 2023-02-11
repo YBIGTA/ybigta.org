fc561ee46dfa006f5a5da79ded9f2905c65a60ba
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-02-11 12:33:57 +0900

    커리큘럼 페이지 이미지 작성

--- src/routes/curriculum/+page.svelte
+++ src/routes/curriculum/+page.svelte
@@ -2,6 +2,8 @@
 	import PageSection from '$lib/layout/PageSection.svelte';
 	import PageTitle from '$lib/layout/PageTitle.svelte';
 	import Seo from '$lib/layout/Seo.svelte';
+	import MainCurriculum from './components/MainCurriculum.svelte';
+	import NewbieCurriculum from './components/NewbieCurriculum.svelte';
 </script>
 
 <Seo
@@ -9,27 +11,27 @@
 	desc="데이터에 대한 다양한 전문성을 갖추고, 이를 활용한 프로젝트를 진행합니다."
 />
 
-<PageTitle>
-	<span slot="title">Curriculum</span>
-	<span slot="desc">
-		데이터에 대한 다양한 경험과 전문성을 갖추고, 이를 활용하여 실제 프로젝트를 진행합니다.
-	</span>
-	<img src="/images/curriculum.jpeg" alt="curriculum" slot="image" />
-</PageTitle>
-<PageSection>
-	<span slot="title">신입 회원</span>
-	<span slot="desc">합격 이후 팀을 배정받기 전까지의 활동입니다.</span>
-</PageSection>
-<PageSection>
-	<span slot="title">정규 회원</span>
-	<span slot="desc">팀을 배정받은 이후의 활동입니다.</span>
-</PageSection>
+<div class="container">
+	<PageTitle>
+		<span slot="title">Curriculum</span>
+		<span slot="desc">
+			데이터에 대한 다양한 경험과 전문성을 갖추고, 이를 활용하여 실제 프로젝트를 진행합니다.
+		</span>
+		<img id="page-cover" src="/images/curriculum/cover.jpeg" alt="curriculum" slot="image" />
+	</PageTitle>
+	<NewbieCurriculum />
+	<MainCurriculum />
+</div>
 <div class="placeholder" />
 
-<style>
-	img {
+<style lang="scss">
+	div.container {
+		max-width: 1200px;
+		margin: 0 auto;
+	}
+	img#page-cover {
 		width: 100%;
-		height: 400px;
+		aspect-ratio: 16 / 6;
 		object-fit: cover;
 		object-position: 50% 40%;
 	}
---
+++ src/routes/curriculum/components/Glossaries.svelte
@@ -0,0 +1,27 @@
+<ul class="glossaries">
+	<slot name="content" />
+</ul>
+
+<style lang="scss">
+	ul.glossaries {
+		margin-top: 2rem;
+		margin-left: 2rem;
+		list-style: disc;
+		padding: 0;
+		font-size: 1.2rem;
+		line-height: 1.2;
+
+		:global(li) {
+			margin-bottom: 1rem;
+			word-break: keep-all;
+
+			&:last-child {
+				margin-bottom: 0;
+			}
+
+			:global(strong) {
+				font-weight: 600;
+			}
+		}
+	}
+</style>
